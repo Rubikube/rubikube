0f9f72b8b1ef46cabd0d74d3cc630e0f66a4b1af
Alexandre Oliveira <alexandre.oliveira@sumware.com.br>
2019-01-19 10:03:24 -0200

    - Kubernetes Client and Show Namespaces

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const Client = require('kubernetes-client').Client
+const config = require('kubernetes-client').config
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viglet Rubikube' });
+router.get('/', async function (req, res, next) {
+  const client = new Client({
+    config: config.fromKubeconfig(),
+    version: '1.9'
+  })
+
+  const namespaces = await client.api.v1.namespaces.get();
+  res.render('index', {
+    title: 'Viglet Rubikube',
+    body : JSON.stringify(namespaces)
+  });
 });
 
 module.exports = router;
